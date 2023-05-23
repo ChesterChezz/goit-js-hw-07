55bdbe932893e7362480436f28de3227c7cf3574
Dmytro Haidai <dimasow12@gmail.com>
2023-05-23 17:52:55 +0300

    cmt2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,37 +3,39 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const ul = document.querySelector(".gallery");
-ul.append(...createGallery());
-let lightbox;
-function createGallery() {
-  return galleryItems.map((element) => {
-    const li = document.createElement("li");
-    li.classList.add("gallery__item");
-    const img = document.createElement("img");
-    const a = document.createElement("a");
-    img.classList.add("gallery__image");
-    img.src = `${element.preview}`;
-    img.alt = `${element.description}`;
-    img.dataset.source = `${element.original}`;
-    a.href = `${element.original}`;
-    a.classList.add("gallery__link");
-    a.addEventListener("click", (event) => {
-      event.preventDefault();
-      lightbox = basicLightbox.create(`<img src="${a.href}" alt="${img.alt}">`);
-      lightbox.show();
-      document.addEventListener("keydown", closeLightBox);
-    });
-    a.appendChild(img);
-    li.appendChild(a);
-    return li;
-  });
-}
 
-//Закриття по натисканню на ESC
-function closeLightBox(event) {
-  if (event.key === "Escape") {
-    lightbox.close();
+const galleryList = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      ` <li class="gallery__item">
+    <a class="gallery__link" href=${original}>
+    <img
+    class="gallery__image"
+    src=${preview}
+    data-source=${original}
+    alt=${description}
+    />
+    </a>
+    </li>`
+  )
+  .join("");
+
+ul.insertAdjacentHTML("beforeend", galleryList);
+
+ul.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
   }
-  //Видалдяю слухача
-  document.removeEventListener("keydown", closeLightBox);
-}
+  const currentImage = e.target.getAttribute("data-source");
+
+  const instance = basicLightbox.create(`
+    <img src="${currentImage}" >`);
+
+  instance.show();
+  ul.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      instance.close();
+    }
+  });
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,31 +3,21 @@ import { galleryItems } from "./gallery-items.js";
 console.log(galleryItems);
 
 const ul = document.querySelector(".gallery");
-ul.append(...createGallery());
 
-function createGallery() {
-  return galleryItems.map((element) => {
-    const li = document.createElement("li");
-    li.classList.add("gallery__item");
-    const img = document.createElement("img");
-    const a = document.createElement("a");
-    img.classList.add("gallery__image");
-    img.src = `${element.preview}`;
-    img.alt = `${element.description}`;
-    a.href = `${element.original}`;
-    a.classList.add("gallery__link");
-    a.addEventListener("click", (event) => {
-      event.preventDefault();
+const galleryList = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+    <a href="${original}" class="gallery__link">
+    <img class="gallery__image" src="${preview}" alt="${description}">
+    </a>
+    </li>`;
+  })
+  .join("");
 
-      var lightbox = new SimpleLightbox(".gallery a", {
-        /* options */
-        captionDelay: 250,
-        captionsData: "alt",
-      });
-      lightbox.open();
-    });
-    a.appendChild(img);
-    li.appendChild(a);
-    return li;
-  });
-}
+ul.insertAdjacentHTML("beforeend", galleryList);
+
+const gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
